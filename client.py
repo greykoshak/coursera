61ef89bb2e6717e717ff41afabeb19521fb48d6d
gk <greykoshak@gmail.com>
2018-12-10 21:31:10 +0200

    put, get - 5-th week

--- client.py
+++ client.py
@@ -15,22 +15,37 @@ class Client:
         self.sock = socket.create_connection((self.host, self.port), self.timeout)
         # self.sock.close()
 
+    # Метод put не возвращает ничего в случае успешной отправки и выбрасывает исключение ClientError в случае неуспешной.
     def put(self, metric, value, timestamp):
         try:
             if timestamp is None:
                 timestamp = str((int(time.time())))
 
-            message = f"put {metric} {value} {timestamp}\n"
-            self.sock.sendall(message.encode("utf-8"))
+            message = f"put {metric} {float(value)} {timestamp}\n"
+            self.sock.send(message.encode("utf-8"))
 
-            data = self.sock.recv(1024).decode()  # Server answer
+            data = self.sock.recv(1024).decode().split("\n")  # Server answer
 
-        except:
-            raise ClientError()
+            if data[0] is not "ok":
+                raise ClientError
 
-        return
+        except socket.timeout:
+            raise ClientError
+
 
     def get(self, metric):
+        try:
+            message = f"get {metric}\n"
+            self.sock.send(message.encode("utf-8"))
+
+            data = ""
+            while True:
+                data += self.sock.recv(1024).decode()  # Server answer
+                if "\n\n" in data:
+                    break
+
+        except socket.timeout:
+            raise ClientError
         return
 
     def close(self):
@@ -58,3 +73,25 @@ def _main():
 
 if __name__ == "__main__":
     _main()
+
+
+
+
+str = b'ok\ntest 0.5 1\ntest 0.4 2\nload 301 3\n\n'
+answer = (str.decode()[3::]).split() # Получился список
+
+print(answer)
+
+dict = {}
+
+for key in answer[::3]:
+    if key in dict:
+        dict[key].append(tuple([3, 4]))
+    else:
+        # print(dict)
+        dict.update({key: [tuple([1, 2])]})
+
+print(dict)
+
+if "load" in dict:
+    print("ok")
